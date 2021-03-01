e6e073c2f1b3aaaab15e90dc9cc43399b1f46fa1
Steven Noonan <steven@uplinklabs.net>
2021-03-01 08:07:04 -0800

    msvc: adding project to integrate into games
    Not using this yet, because the spatialized audio feature isn't yet
    implemented, but I'm getting a head start.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ miniaudio_engine.c
@@ -0,0 +1,8 @@
+#define MA_IMPLEMENTATION
+#define MA_NO_WAV
+#define MA_NO_MP3
+#define MA_NO_FLAC
+#define MA_DEBUG_OUTPUT
+#define MA_LOG_LEVEL_VERBOSE
+#include "miniaudio.h"
+#include "miniaudio_engine.h"
