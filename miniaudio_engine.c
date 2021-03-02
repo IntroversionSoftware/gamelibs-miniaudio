7fa535db3a1a4c6ea974f9ac113109ec8424ebb9
Steven Noonan <steven@uplinklabs.net>
2021-03-02 11:19:42 -0800

    miniaudio_engine.c: add vorbis decoding
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- miniaudio_engine.c
+++ miniaudio_engine.c
@@ -1,8 +1,13 @@
+#define STB_VORBIS_HEADER_ONLY
+#include "extras/stb_vorbis.c"
+
 #define MA_IMPLEMENTATION
-#define MA_NO_WAV
 #define MA_NO_MP3
 #define MA_NO_FLAC
 #define MA_DEBUG_OUTPUT
-#define MA_LOG_LEVEL_VERBOSE
+#define MA_LOG_LEVEL MA_LOG_LEVEL_VERBOSE
 #include "miniaudio.h"
 #include "miniaudio_engine.h"
+
+#undef STB_VORBIS_HEADER_ONLY
+#include "extras/stb_vorbis.c"
