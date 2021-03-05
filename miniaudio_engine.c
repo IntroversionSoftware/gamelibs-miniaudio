025e66ccc33ed174eb74311a81e33ce645840d0a
Steven Noonan <steven@uplinklabs.net>
2021-03-05 12:10:53 -0800

    disable custom audio backend
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- miniaudio_engine.c
+++ miniaudio_engine.c
@@ -1,12 +1,13 @@
 #define MA_DEBUG_OUTPUT
 #define MA_LOG_LEVEL MA_LOG_LEVEL_VERBOSE
+#define MA_NO_MP3
+#define MA_NO_FLAC
+#define MA_NO_CUSTOM
 
 #include "miniaudio.h"
 #include "miniaudio_vorbis.h"
 
 #define MINIAUDIO_IMPLEMENTATION
-#define MA_NO_MP3
-#define MA_NO_FLAC
 #include "miniaudio.h"
 #include "miniaudio_engine.h"
 
