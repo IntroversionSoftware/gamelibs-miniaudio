5f783b0abd0a254c0ca98266b69fddf9becf8b3e
Steven Noonan <steven@uplinklabs.net>
2021-03-05 20:15:52 -0800

    disable encoding, prefer AVX2
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- miniaudio_engine.c
+++ miniaudio_engine.c
@@ -1,8 +1,13 @@
+#ifdef _DEBUG
 #define MA_DEBUG_OUTPUT
 #define MA_LOG_LEVEL MA_LOG_LEVEL_VERBOSE
+#endif
+
 #define MA_NO_MP3
 #define MA_NO_FLAC
 #define MA_NO_CUSTOM
+#define MA_NO_ENCODING
+#define MA_PREFER_AVX2
 
 #include "miniaudio.h"
 #include "miniaudio_vorbis.h"
