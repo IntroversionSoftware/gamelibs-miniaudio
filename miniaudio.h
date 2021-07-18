cdc3bad8e5d199fd03aa05be37ca73a8b87ffb66
Steven Noonan <steven@uplinklabs.net>
2021-07-17 18:41:17 -0700

    miniaudio: never treat MinGW builds as UWP
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- miniaudio.h
+++ miniaudio.h
@@ -3723,7 +3723,7 @@ typedef ma_uint16 wchar_t;
 /* Platform/backend detection. */
 #ifdef _WIN32
     #define MA_WIN32
-    #if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_PC_APP || WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP)
+    #if !defined(__MINGW64__) && defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_PC_APP || WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP)
         #define MA_WIN32_UWP
     #else
         #define MA_WIN32_DESKTOP
