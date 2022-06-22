f59223f12111463ce117d9c422d22dcb6a6349a2
Steven Noonan <steven@uplinklabs.net>
2022-06-22 00:46:52 -0700

    build: add dependency on tracy
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- miniaudio.h
+++ miniaudio.h
@@ -3674,6 +3674,8 @@ See below for some tips on improving performance.
   is due to 64-bit file APIs not being available.
 */
 
+#include "TracyC.h"
+
 #ifndef miniaudio_h
 #define miniaudio_h
 
@@ -19156,6 +19158,8 @@ static ma_thread_result MA_THREADCALL ma_device_thread__null(void* pData)
     ma_device* pDevice = (ma_device*)pData;
     MA_ASSERT(pDevice != NULL);
 
+    TracyCSetThreadName("MiniAudio null device thread");
+
     for (;;) {  /* Keep the thread alive until the device is uninitialized. */
         ma_uint32 operation;
 
@@ -20878,6 +20882,8 @@ static ma_thread_result MA_THREADCALL ma_context_command_thread__wasapi(void* pU
     ma_result result;
     ma_context* pContext = (ma_context*)pUserData;
     MA_ASSERT(pContext != NULL);
+
+    TracyCSetThreadName("MiniAudio WASAPI command thread");
 
     for (;;) {
         ma_context_command__wasapi cmd;
@@ -40037,6 +40043,8 @@ static ma_thread_result MA_THREADCALL ma_worker_thread(void* pData)
     ma_device* pDevice = (ma_device*)pData;
     MA_ASSERT(pDevice != NULL);
 
+    TracyCSetThreadName("MiniAudio worker thread");
+
 #ifdef MA_WIN32
     ma_CoInitializeEx(pDevice->pContext, NULL, MA_COINIT_VALUE);
 #endif
@@ -40264,6 +40272,8 @@ static ma_thread_result MA_THREADCALL ma_device_job_thread_entry(void* pUserData
 {
     ma_device_job_thread* pJobThread = (ma_device_job_thread*)pUserData;
     MA_ASSERT(pJobThread != NULL);
+
+    TracyCSetThreadName("MiniAudio device job thread");
 
     for (;;) {
         ma_result result;
@@ -66107,6 +66117,8 @@ static ma_thread_result MA_THREADCALL ma_resource_manager_job_thread(void* pUser
 {
     ma_resource_manager* pResourceManager = (ma_resource_manager*)pUserData;
     MA_ASSERT(pResourceManager != NULL);
+
+    TracyCSetThreadName("MiniAudio resource manager job thread");
 
     for (;;) {
         ma_result result;
