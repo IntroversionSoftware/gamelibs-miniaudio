1330fed660a23f3e1c1ee95bc7779db077e60c0f
Steven Noonan <steven@uplinklabs.net>
2023-05-16 15:26:04 -0700

    disable inlining for string copy/comparison functions
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- miniaudio.h
+++ miniaudio.h
@@ -18425,7 +18425,7 @@ typedef LONG    (WINAPI * MA_PFN_RegQueryValueExA)(HKEY hKey, const char* lpValu
 #endif  /* MA_WIN32_DESKTOP */
 
 
-MA_API size_t ma_strlen_WCHAR(const WCHAR* str)
+MA_API MA_NO_INLINE size_t ma_strlen_WCHAR(const WCHAR* str)
 {
     size_t len = 0;
     while (str[len] != '\0') {
@@ -18435,7 +18435,7 @@ MA_API size_t ma_strlen_WCHAR(const WCHAR* str)
     return len;
 }
 
-MA_API int ma_strcmp_WCHAR(const WCHAR *s1, const WCHAR *s2)
+MA_API MA_NO_INLINE int ma_strcmp_WCHAR(const WCHAR *s1, const WCHAR *s2)
 {
     while (*s1 != '\0' && *s1 == *s2) {
         s1 += 1;
@@ -18445,7 +18445,7 @@ MA_API int ma_strcmp_WCHAR(const WCHAR *s1, const WCHAR *s2)
     return *s1 - *s2;
 }
 
-MA_API int ma_strcpy_s_WCHAR(WCHAR* dst, size_t dstCap, const WCHAR* src)
+MA_API MA_NO_INLINE int ma_strcpy_s_WCHAR(WCHAR* dst, size_t dstCap, const WCHAR* src)
 {
     size_t i;
 
